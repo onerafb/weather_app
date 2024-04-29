ca5ec4782e2757f093820906e1c9186b415f4f44
UNIT <nox@localhost.localdomain>
2024-04-29 18:30:51 +0530

    third commit

--- app/components/Input.jsx
+++ app/components/Input.jsx
@@ -1,8 +1,6 @@
 "use client";
 
-import styles from "../styles/input.module.css"
-
-
+import styles from "../styles/input.module.css";
 
 const Input = ({ handleSearch, setLocation }) => {
   return (
--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=8dba1270f9ca41689af174404242704&q=${location}&days=7&aqi=yes&alerts=yes`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=8dba1270f9ca41689af174404242704&q=${location}&days=7&aqi=yes&alerts=yes`;
 
   const handleSearch = async (e) => {
     if (e.key === "Enter") {
